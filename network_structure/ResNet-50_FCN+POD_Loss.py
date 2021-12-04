83aeb755dad6c9a466299a71dc657b65854bff98
TianYuZu <81952085+TianYuZu@users.noreply.github.com>
2021-12-04 19:05:24 +0800

    Add files via upload

---
+++ network_structure/ResNet-50_FCN+POD_Loss.py
@@ -0,0 +1,140 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from train_test.utils import CosineLinear_PEDCC
+
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1, shortcut=None):
+        super(BasicBlock, self).__init__()
+        self.layers = nn.Sequential(
+            conv3x3(in_planes, planes, stride),
+            nn.BatchNorm2d(planes),
+            nn.ReLU(True),
+            conv3x3(planes, planes),
+            nn.BatchNorm2d(planes),
+        )
+        self.shortcut = shortcut
+
+    def forward(self, x):
+        residual = x
+        y = self.layers(x)
+        if self.shortcut:
+            residual = self.shortcut(x)
+        y += residual
+        y = F.relu(y)
+        return y
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1, shortcut=None):
+        super(Bottleneck, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(in_planes, planes, kernel_size=1, bias=False),
+            nn.BatchNorm2d(planes),
+            nn.ReLU(True),
+            nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(planes),
+            nn.ReLU(True),
+            nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False),
+            nn.BatchNorm2d(planes * 4),
+        )
+        self.shortcut = shortcut
+
+    def forward(self, x):
+        residual = x
+        y = self.layers(x)
+        if self.shortcut:
+            residual = self.shortcut(x)
+        y += residual
+        y = F.relu(y)
+        return y
+class Bottleneck1(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes,  classes_num=1000, stride=1, shortcut=False):
+        super(Bottleneck1, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(in_planes, planes, kernel_size=1, bias=False),
+            nn.BatchNorm2d(planes),
+            nn.ReLU(True),
+            nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(planes),
+            nn.ReLU(True),
+            nn.Conv2d(planes, classes_num, kernel_size=1, bias=False),
+            nn.BatchNorm2d(classes_num),
+        )
+        self.shortcut = shortcut
+        self.classNum = classes_num
+    def forward(self, x):
+        residual = x
+        y = self.layers(x)
+        if self.shortcut:
+            residual = x[:, 0:self.classNum, :, :]
+        y += residual
+        return y
+
+
+class ResNet50_FCN_Plus_POD_Loss(nn.Module):
+    def __init__(self, block, nblocks, num_classes=1000):
+        super(ResNet50_FCN, self).__init__()
+        self.in_planes = 64
+        self.pre_layers = nn.Sequential(
+            conv3x3(3, 64),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+        )
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, nblocks[0])
+        self.layer2 = self._make_layer(block, 128, nblocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, nblocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, nblocks[3], stride=2, removefc=True, classes_num=num_classes, block1=Bottleneck1)
+        self.out = CosineLinear_PEDCC(feature_size, num_classes)
+        self.avgpool = nn.AvgPool2d(4)
+
+
+    def l2_norm(self, input):
+        x_norm = torch.norm(input, p=2, dim=1, keepdim=True).clamp(min=1e-12)
+        xout = torch.div(input, x_norm)
+        return xout
+
+
+    def _make_layer(self, block, planes, nblocks, stride=1, removefc=False, classes_num=1000, block1=Bottleneck1):
+        shortcut = None
+        if stride != 1 or self.in_planes != planes * block.expansion:
+            shortcut = nn.Sequential(
+                nn.Conv2d(self.in_planes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+        layers = []
+        layers.append(block(self.in_planes, planes, stride, shortcut))
+        self.in_planes = planes * block.expansion
+        if removefc:
+            nblocks = nblocks - 1
+        for i in range(1, nblocks):
+            layers.append(block(self.in_planes, planes))
+        if removefc:
+            layers.append(block1(self.in_planes, planes, classes_num=classes_num, stride=1, shortcut=True))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.pre_layers(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x_norm = self.l2_norm(x)
+        out = self.out(x_norm)
+        return out, x_norm
