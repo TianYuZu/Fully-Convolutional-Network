a54bb9231927daf1511cba0dc2baf759ff974456
TianYuZu <81952085+TianYuZu@users.noreply.github.com>
2021-12-04 19:12:20 +0800

    Update config.py

--- conf/config.py
+++ conf/config.py
@@ -7,6 +7,7 @@ HOME = os.path.expanduser("~")
 # different DATA set configs
 CIFAR10 = {
     'num_classes': 10,
+    'feature_sizes': 256,
     'max_epoch': 100,
     'LR': 1e-1,
     'lr_steps': (0, 30, 60, 90),
@@ -15,6 +16,7 @@ CIFAR10 = {
 }
 CIFAR100 = {
     'num_classes': 100,
+    'feature_sizes': 512,
     'max_epoch': 200,
     'LR': 1e-1,
     'lr_steps': (0, 50, 100, 150),
@@ -23,6 +25,7 @@ CIFAR100 = {
 }
 TinyImageNet = {
     'num_classes': 200,
+    'feature_sizes': 512,
     'max_epoch': 100,
     'LR': 1e-1,
     'lr_steps': (0, 30, 60, 90),
@@ -31,6 +34,7 @@ TinyImageNet = {
 }
 FaceScrubs = {
     'num_classes': 100,
+    'feature_sizes': 512,
     'max_epoch': 100,
     'LR': 1e-1,
     'lr_steps': (0, 30, 60, 90),
@@ -39,6 +43,7 @@ FaceScrubs = {
 }
 ImageNet1000 = {
     'num_classes': 1000,
+    'feature_sizes': 2048, #Varies according to the number of classes, the corresponding relationship between them has been given in our paper.
     'max_epoch': 20,
     'LR': xxx,   # Set according to the specific situation
     'lr_steps': (xxx, xxx, xxx, xxx),   #Set according to the specific situation
@@ -47,6 +52,7 @@ ImageNet1000 = {
 }
 miniImageNet = {
     'num_classes': 100,
+    'feature_sizes': 512,
     'max_epoch': 100,
     'LR': 1e-1,
     'lr_steps': (0, 30, 60, 90),
